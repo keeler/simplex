f63d5c762c840e9b74fcb3f6e7c5210480f18d6b
Keeler Russell <keeler-russell@ku.edu>
2012-04-04 04:58:43 -0500

    Added debug assertion macros for documenting assumptions.

---
+++ Debug.hpp
@@ -0,0 +1,27 @@
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+// Change this to ASSERTIONS_OFF to turn them off
+#define ASSERTIONS_ON 1
+
+/***************************************
+* Assertion Macros
+***************************************/
+#if ASSERTIONS_ON
+	// Asserts the condition, and terminates the program if false.
+	#define fatalAssert( X, MSG ) if( !( X ) ) { std::cout << "Fatal assertion failed: " << #X << ", File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
+	// Asserts the condition, but does not terminate the program if false.
+	#define softAssert( X, MSG ) if( !( X ) ) { std::cout << "Soft assertion failed: " << #X << ", File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; }
+	// A fatal assert that always occurs if executed.
+	#define alwaysAssert( MSG ) { std::cout << "Always assert here: " << "File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
+#else
+	#define fatalAssert( X, MSG ) {}
+	#define softAssert( X, MSG ) {}
+	#define alwaysAssert( MSG ) {}
+#endif
+
+#endif
