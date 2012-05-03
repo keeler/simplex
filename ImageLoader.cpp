830bbcc9b3c127719dfdd95c84ce4046704db1b2
Keeler Russell <keeler-russell@ku.edu>
2012-05-03 01:46:11 -0500

    Implemented bitmap saving function.
    Can now save an array of RGB triples to a 24 bits per pixel, uncompressed bitmap file.

--- ImageLoader.cpp
+++ ImageLoader.cpp
@@ -54,3 +54,61 @@ void loadBitmap( const std::string & filename, unsigned char *& pixels, int & wi
 
 	infile.close();
 }
+
+void saveBitmap( const std::string & filename, unsigned char *pixels, int width, int height )
+{
+	std::ofstream outfile;
+
+	outfile.open( filename.c_str(), std::ios::out | std::ios::binary );
+	fatalAssert( !outfile.fail(), "Could not open \'" + filename + "\'." );
+
+	// Write magic number
+	BmpFileMagic bmpFileMagic;
+	bmpFileMagic.magicNumber[0] = 'B';
+	bmpFileMagic.magicNumber[1] = 'M';
+	outfile.write( bmpFileMagic.magicNumber, 2 );
+
+	// Write file header
+	BmpFileHeader bmpFileHeader;
+	bmpFileHeader.fileSize = sizeof( BmpFileMagic ) + sizeof( BmpFileHeader ) + sizeof( BmpInfoHeader ) + width * height * 3;		// Size of bitmap file in bytes
+	bmpFileHeader.reserved1 = 0;
+	bmpFileHeader.reserved2 = 0;
+	bmpFileHeader.dataOffset = sizeof( BmpFileMagic ) + sizeof( BmpFileHeader ) + sizeof( BmpInfoHeader );		// Starting address of byte where pixel data starts
+	outfile.write( (char *)&bmpFileHeader, sizeof( BmpFileHeader ) );
+
+	// Write info header
+	BmpInfoHeader bmpInfoHeader;
+	bmpInfoHeader.headerSize = 40;
+	bmpInfoHeader.bitmapWidth = width;
+	bmpInfoHeader.bitmapHeight = height;
+	bmpInfoHeader.numColorPlanes = 1;
+	bmpInfoHeader.bitsPerPixel = 24;
+	bmpInfoHeader.compressionType = 0;
+	bmpInfoHeader.rawImageDataSize = width * height * 3;
+	bmpInfoHeader.horizontalResolution = 2835;    // 0x130B0000
+	bmpInfoHeader.verticalResolution = 2835;
+	bmpInfoHeader.numColorsInPalette = 0;
+	bmpInfoHeader.numImportantColors = 0;
+	outfile.write( (char *)&bmpInfoHeader, sizeof( BmpInfoHeader ) );
+
+	// Write pixel data to file
+	int rowSize = 4 * ceil( ( bmpInfoHeader.bitsPerPixel * width ) / 32.0f );    // In bytes
+	int pixelArraySize = rowSize * abs( height );
+	char *rawPixelData = new char[pixelArraySize];
+
+	// Transform the RGB triples from OpenGL format to bitmap format
+	for( int y = 0; y < height; y++ )
+	{
+		for( int x = 0; x < width; x++ )
+		{
+			for( int c = 0; c < 3; c++ )
+			{
+				rawPixelData[rowSize * y + 3 * x + ( 2 - c )] = pixels[3 * ( width * y + x ) + c];
+			}
+		}
+	}
+
+	outfile.write( rawPixelData, pixelArraySize );
+
+	outfile.close();
+}
