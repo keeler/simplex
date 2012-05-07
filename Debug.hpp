4416149db15d40042f3cd0141912a54050bd1db1
Keeler Russell <keeler-russell@ku.edu>
2012-05-06 23:17:25 -0500

    Minor change to format of assert messages to make them more clear.
    Also easier to spot.

--- Debug.hpp
+++ Debug.hpp
@@ -13,11 +13,11 @@
 ***************************************/
 #if ASSERTIONS_ON
 	// Asserts the condition, and terminates the program if false.
-	#define fatalAssert( X, MSG ) if( !( X ) ) { std::cout << "Fatal assertion failed: " << #X << ", File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
+	#define fatalAssert( X, MSG ) if( !( X ) ) { std::cout << "Fatal assertion failed: " << std::endl << "\tContidion: " << #X << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
 	// Asserts the condition, but does not terminate the program if false.
-	#define softAssert( X, MSG ) if( !( X ) ) { std::cout << "Soft assertion failed: " << #X << ", File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; }
+	#define softAssert( X, MSG ) if( !( X ) ) { std::cout << "Soft assertion failed: " << std::endl << "\tContidion: " << #X << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; }
 	// A fatal assert that always occurs if executed.
-	#define alwaysAssert( MSG ) { std::cout << "Always assert here: " << "File: " << __FILE__ << ", Line: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
+	#define alwaysAssert( MSG ) { std::cout << "Always assert here: " << std::endl << "\tFile: " << __FILE__ << std::endl << "\tLine: " << __LINE__ << std::endl << "\tMessage: " << MSG << std::endl; exit( -1 ); }
 #else
 	#define fatalAssert( X, MSG ) {}
 	#define softAssert( X, MSG ) {}
